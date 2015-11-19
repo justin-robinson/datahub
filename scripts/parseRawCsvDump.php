96a01134622486788b1b58ae1bc67c2a54ed381f
dbullard <dbullard@bizjournals.com>
2015-11-19 11:16:16 -0500

    working on formatting country codes

--- scripts/parseRawCsvDump.php
+++ scripts/parseRawCsvDump.php
@@ -2,52 +2,356 @@
 /**
  *  Let's get rid of all of the crap and prepare the rest for import!
  *  requires the path : /usr/local/bizj/dHubWorkSpace
+ *  requires something to parse as well
  */
-if (($handle = fopen('/usr/local/bizj/dHubWorkSpace/bridgetree_dump', "r")) !== false) {
-    $parsedFile   = fopen('/usr/local/bizj/dHubWorkSpace/parsed.csv', 'w');
-    $headerRaw    = fgetcsv($handle, 0, "|");
-    $headerSource = [];
-    foreach ($headerRaw as $key => $value) {
-        $headerSource[$key] = str_replace('|', '', $value);
-    }
+use Entity\Proxy\__CG__\Entity\NascarIllustrated\CountryList;
 
-    $headerProcessed = [];
+/**
+ * @todo lookup country codes
+ * @todo generate country codes if they aren't present
+ */
 
-    $colsToKeep = [
-        'CompanyId',
-        'Name',
-        'Ticker',
-        'TickerExchange',
-        'Addr1',
-        'Addr2',
-        'City',
-        'State',
-        'ZipCode',
-        'Country',
-        'OfficePhone1',
-        'Url',
-        'LastStoryDate',
+
+/**
+ * stage one filter out records that we don't want
+ */
+//if (($handle = fopen('/usr/local/bizj/dHubWorkSpace/bridgetree_dump', "r")) !== false) {
+//    $parsedFile   = fopen('/usr/local/bizj/dHubWorkSpace/parsed.csv', 'w');
+//    $headerRaw    = fgetcsv($handle, 0, "|");
+//    $headerSource = [];
+//    foreach ($headerRaw as $key => $value) {
+//        $headerSource[$key] = str_replace('|', '', $value);
+//    }
+//
+//    $headerProcessed = [];
+//
+//    $colsToKeep = [
+//        'CompanyId',
+//        'Name',
+//        'Ticker',
+//        'TickerExchange',
+//        'Addr1',
+//        'Addr2',
+//        'City',
+//        'State',
+//        'ZipCode',
+//        'Country',
+//        'OfficePhone1',
+//        'Url',
+//        'LastStoryDate',
+//    ];
+//    foreach ($headerSource as $k => $value) {
+//        if (in_array($value, $colsToKeep)) {
+//            $headerProcessed[$k] = $value;
+//        }
+//    }
+//
+//    // write the header row
+//    fputcsv($parsedFile, $headerProcessed);
+//
+//    while ($recordRaw = fgetcsv($handle, 0, '|')) {
+//        $recordProcessed = [];
+//        foreach ($recordRaw as $k => $v) {
+//            if (array_key_exists($k, $headerProcessed)) {
+//                $recordProcessed[$k] = trim($v, '|');
+//            }
+//        }
+//        if (!empty($recordProcessed[52])) {
+//            fputcsv($parsedFile, $recordProcessed);
+//        }
+//    }
+//    fclose($parsedFile);
+//    fclose($handle);
+//}
+
+/**
+ * stage 2 normalize
+ */
+
+if (($parsed = fopen('/usr/local/bizj/dHubWorkSpace/parsed.csv', "r")) !== false) {
+
+    $normalizedFile = fopen('/usr/local/bizj/dHubWorkSpace/normalized.csv', 'w');
+    $referenceCols  = fgetcsv($parsed, 0, ",");
+
+    fputcsv($normalizedFile, $referenceCols);
+
+    /**
+     * $referenceCols as of 11-19
+     * [
+     *     [0]  => "CompanyId"
+     *     [1]  => "Name"
+     *     [2]  => "Ticker"
+     *     [3]  => "TickerExchange"
+     *     [4]  => "Addr1"
+     *     [5]  => "Addr2"
+     *     [6]  => "City"
+     *     [7]  => "State"
+     *     [8]  => "ZipCode"
+     *     [9]  => "Country"
+     *     [10] => "OfficePhone1"
+     *     [11] => "Url"
+     *     [12] => "LastStoryDate"
+     * ]
+     */
+
+
+    $countries = [
+        'AF' => 'AFGHANISTAN',
+        'AL' => 'ALBANIA',
+        'DZ' => 'ALGERIA',
+        'AS' => 'AMERICAN SAMOA',
+        'AD' => 'ANDORRA',
+        'AO' => 'ANGOLA',
+        'AI' => 'ANGUILLA',
+        'AQ' => 'ANTARCTICA',
+        'AG' => 'ANTIGUA AND BARBUDA',
+        'AR' => 'ARGENTINA',
+        'AM' => 'ARMENIA',
+        'AW' => 'ARUBA',
+        'AU' => 'AUSTRALIA',
+        'AT' => 'AUSTRIA',
+        'AZ' => 'AZERBAIJAN',
+        'BS' => 'BAHAMAS',
+        'BH' => 'BAHRAIN',
+        'BD' => 'BANGLADESH',
+        'BB' => 'BARBADOS',
+        'BY' => 'BELARUS',
+        'BE' => 'BELGIUM',
+        'BZ' => 'BELIZE',
+        'BJ' => 'BENIN',
+        'BM' => 'BERMUDA',
+        'BT' => 'BHUTAN',
+        'BO' => 'BOLIVIA',
+        'BA' => 'BOSNIA AND HERZEGOVINA',
+        'BW' => 'BOTSWANA',
+        'BV' => 'BOUVET ISLAND',
+        'BR' => 'BRAZIL',
+        'IO' => 'BRITISH INDIAN OCEAN TERRITORY',
+        'BN' => 'BRUNEI DARUSSALAM',
+        'BG' => 'BULGARIA',
+        'BF' => 'BURKINA FASO',
+        'BI' => 'BURUNDI',
+        'KH' => 'CAMBODIA',
+        'CM' => 'CAMEROON',
+        'CA' => 'CANADA',
+        'CV' => 'CAPE VERDE',
+        'KY' => 'CAYMAN ISLANDS',
+        'CF' => 'CENTRAL AFRICAN REPUBLIC',
+        'TD' => 'CHAD',
+        'CL' => 'CHILE',
+        'CN' => 'CHINA',
+        'CX' => 'CHRISTMAS ISLAND',
+        'CC' => 'COCOS (KEELING) ISLANDS',
+        'CO' => 'COLOMBIA',
+        'KM' => 'COMOROS',
+        'CG' => 'CONGO',
+        'CD' => 'CONGO, THE DEMOCRATIC REPUBLIC OF THE',
+        'CK' => 'COOK ISLANDS',
+        'CR' => 'COSTA RICA',
+        'CI' => 'COTE D IVOIRE',
+        'HR' => 'CROATIA',
+        'CU' => 'CUBA',
+        'CY' => 'CYPRUS',
+        'CZ' => 'CZECH REPUBLIC',
+        'DK' => 'DENMARK',
+        'DJ' => 'DJIBOUTI',
+        'DM' => 'DOMINICA',
+        'DO' => 'DOMINICAN REPUBLIC',
+        'TP' => 'EAST TIMOR',
+        'EC' => 'ECUADOR',
+        'EG' => 'EGYPT',
+        'SV' => 'EL SALVADOR',
+        'GQ' => 'EQUATORIAL GUINEA',
+        'ER' => 'ERITREA',
+        'EE' => 'ESTONIA',
+        'ET' => 'ETHIOPIA',
+        'FK' => 'FALKLAND ISLANDS (MALVINAS)',
+        'FO' => 'FAROE ISLANDS',
+        'FJ' => 'FIJI',
+        'FI' => 'FINLAND',
+        'FR' => 'FRANCE',
+        'GF' => 'FRENCH GUIANA',
+        'PF' => 'FRENCH POLYNESIA',
+        'TF' => 'FRENCH SOUTHERN TERRITORIES',
+        'GA' => 'GABON',
+        'GM' => 'GAMBIA',
+        'GE' => 'GEORGIA',
+        'DE' => 'GERMANY',
+        'GH' => 'GHANA',
+        'GI' => 'GIBRALTAR',
+        'GR' => 'GREECE',
+        'GL' => 'GREENLAND',
+        'GD' => 'GRENADA',
+        'GP' => 'GUADELOUPE',
+        'GU' => 'GUAM',
+        'GT' => 'GUATEMALA',
+        'GN' => 'GUINEA',
+        'GW' => 'GUINEA-BISSAU',
+        'GY' => 'GUYANA',
+        'HT' => 'HAITI',
+        'HM' => 'HEARD ISLAND AND MCDONALD ISLANDS',
+        'VA' => 'HOLY SEE (VATICAN CITY STATE)',
+        'HN' => 'HONDURAS',
+        'HK' => 'HONG KONG',
+        'HU' => 'HUNGARY',
+        'IS' => 'ICELAND',
+        'IN' => 'INDIA',
+        'ID' => 'INDONESIA',
+        'IR' => 'IRAN, ISLAMIC REPUBLIC OF',
+        'IQ' => 'IRAQ',
+        'IE' => 'IRELAND',
+        'IL' => 'ISRAEL',
+        'IT' => 'ITALY',
+        'JM' => 'JAMAICA',
+        'JP' => 'JAPAN',
+        'JO' => 'JORDAN',
+        'KZ' => 'KAZAKSTAN',
+        'KE' => 'KENYA',
+        'KI' => 'KIRIBATI',
+        'KP' => 'KOREA DEMOCRATIC PEOPLES REPUBLIC OF',
+        'KR' => 'KOREA REPUBLIC OF',
+        'KW' => 'KUWAIT',
+        'KG' => 'KYRGYZSTAN',
+        'LA' => 'LAO PEOPLES DEMOCRATIC REPUBLIC',
+        'LV' => 'LATVIA',
+        'LB' => 'LEBANON',
+        'LS' => 'LESOTHO',
+        'LR' => 'LIBERIA',
+        'LY' => 'LIBYAN ARAB JAMAHIRIYA',
+        'LI' => 'LIECHTENSTEIN',
+        'LT' => 'LITHUANIA',
+        'LU' => 'LUXEMBOURG',
+        'MO' => 'MACAU',
+        'MK' => 'MACEDONIA, THE FORMER YUGOSLAV REPUBLIC OF',
+        'MG' => 'MADAGASCAR',
+        'MW' => 'MALAWI',
+        'MY' => 'MALAYSIA',
+        'MV' => 'MALDIVES',
+        'ML' => 'MALI',
+        'MT' => 'MALTA',
+        'MH' => 'MARSHALL ISLANDS',
+        'MQ' => 'MARTINIQUE',
+        'MR' => 'MAURITANIA',
+        'MU' => 'MAURITIUS',
+        'YT' => 'MAYOTTE',
+        'MX' => 'MEXICO',
+        'FM' => 'MICRONESIA, FEDERATED STATES OF',
+        'MD' => 'MOLDOVA, REPUBLIC OF',
+        'MC' => 'MONACO',
+        'MN' => 'MONGOLIA',
+        'MS' => 'MONTSERRAT',
+        'MA' => 'MOROCCO',
+        'MZ' => 'MOZAMBIQUE',
+        'MM' => 'MYANMAR',
+        'NA' => 'NAMIBIA',
+        'NR' => 'NAURU',
+        'NP' => 'NEPAL',
+        'NL' => 'NETHERLANDS',
+        'AN' => 'NETHERLANDS ANTILLES',
+        'NC' => 'NEW CALEDONIA',
+        'NZ' => 'NEW ZEALAND',
+        'NI' => 'NICARAGUA',
+        'NE' => 'NIGER',
+        'NG' => 'NIGERIA',
+        'NU' => 'NIUE',
+        'NF' => 'NORFOLK ISLAND',
+        'MP' => 'NORTHERN MARIANA ISLANDS',
+        'NO' => 'NORWAY',
+        'OM' => 'OMAN',
+        'PK' => 'PAKISTAN',
+        'PW' => 'PALAU',
+        'PS' => 'PALESTINIAN TERRITORY, OCCUPIED',
+        'PA' => 'PANAMA',
+        'PG' => 'PAPUA NEW GUINEA',
+        'PY' => 'PARAGUAY',
+        'PE' => 'PERU',
+        'PH' => 'PHILIPPINES',
+        'PN' => 'PITCAIRN',
+        'PL' => 'POLAND',
+        'PT' => 'PORTUGAL',
+        'PR' => 'PUERTO RICO',
+        'QA' => 'QATAR',
+        'RE' => 'REUNION',
+        'RO' => 'ROMANIA',
+        'RU' => 'RUSSIAN FEDERATION',
+        'RW' => 'RWANDA',
+        'SH' => 'SAINT HELENA',
+        'KN' => 'SAINT KITTS AND NEVIS',
+        'LC' => 'SAINT LUCIA',
+        'PM' => 'SAINT PIERRE AND MIQUELON',
+        'VC' => 'SAINT VINCENT AND THE GRENADINES',
+        'WS' => 'SAMOA',
+        'SM' => 'SAN MARINO',
+        'ST' => 'SAO TOME AND PRINCIPE',
+        'SA' => 'SAUDI ARABIA',
+        'SN' => 'SENEGAL',
+        'SC' => 'SEYCHELLES',
+        'SL' => 'SIERRA LEONE',
+        'SG' => 'SINGAPORE',
+        'SK' => 'SLOVAKIA',
+        'SI' => 'SLOVENIA',
+        'SB' => 'SOLOMON ISLANDS',
+        'SO' => 'SOMALIA',
+        'ZA' => 'SOUTH AFRICA',
+        'GS' => 'SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS',
+        'ES' => 'SPAIN',
+        'LK' => 'SRI LANKA',
+        'SD' => 'SUDAN',
+        'SR' => 'SURINAME',
+        'SJ' => 'SVALBARD AND JAN MAYEN',
+        'SZ' => 'SWAZILAND',
+        'SE' => 'SWEDEN',
+        'CH' => 'SWITZERLAND',
+        'SY' => 'SYRIAN ARAB REPUBLIC',
+        'TW' => 'TAIWAN, PROVINCE OF CHINA',
+        'TJ' => 'TAJIKISTAN',
+        'TZ' => 'TANZANIA, UNITED REPUBLIC OF',
+        'TH' => 'THAILAND',
+        'TG' => 'TOGO',
+        'TK' => 'TOKELAU',
+        'TO' => 'TONGA',
+        'TT' => 'TRINIDAD AND TOBAGO',
+        'TN' => 'TUNISIA',
+        'TR' => 'TURKEY',
+        'TM' => 'TURKMENISTAN',
+        'TC' => 'TURKS AND CAICOS ISLANDS',
+        'TV' => 'TUVALU',
+        'UG' => 'UGANDA',
+        'UA' => 'UKRAINE',
+        'AE' => 'UNITED ARAB EMIRATES',
+        'GB' => 'UNITED KINGDOM',
+        'US' => 'UNITED STATES',
+        'UM' => 'UNITED STATES MINOR OUTLYING ISLANDS',
+        'UY' => 'URUGUAY',
+        'UZ' => 'UZBEKISTAN',
+        'VU' => 'VANUATU',
+        'VE' => 'VENEZUELA',
+        'VN' => 'VIET NAM',
+        'VG' => 'VIRGIN ISLANDS, BRITISH',
+        'VI' => 'VIRGIN ISLANDS, U.S.',
+        'WF' => 'WALLIS AND FUTUNA',
+        'EH' => 'WESTERN SAHARA',
+        'YE' => 'YEMEN',
+        'YU' => 'YUGOSLAVIA',
+        'ZM' => 'ZAMBIA',
+        'ZW' => 'ZIMBABWE',
     ];
-    foreach ($headerSource as $k => $value) {
-        if (in_array($value, $colsToKeep)) {
-            $headerProcessed[$k] = $value;
-        }
-    }
 
-    // write the header row
-    fputcsv($parsedFile, $headerProcessed);
 
-    while ($recordRaw = fgetcsv($handle, 0, '|')) {
-        $recordProcessed = [];
-        foreach ($recordRaw as $k => $v) {
-            if (array_key_exists($k, $headerProcessed)) {
-                $recordProcessed[$k] = trim($v, '|');
+    // country code conversion
+    while ($record = fgetcsv($parsed, 0, ',')) {
+        if (!empty($record[9])) {
+            $raw       = $record[9];
+            $processed = strtoupper($raw);
+
+            if (in_array($processed, $countries)){
+                $record[9] = array_search($processed, $countries);
             }
+
         }
-        if (!empty($recordProcessed[52])) {
-            fputcsv($parsedFile, $recordProcessed);
-        }
+        fputcsv($normalizedFile, $record);
     }
-    fclose($parsedFile);
-    fclose($handle);
+fclose($normalizedFile);
+ fclose($parsed);
+
 }
