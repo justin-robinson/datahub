43306e2168122b230b64b0d35ce6bbf1cff42aea
dbullard <dbullard@bizjournals.com>
2015-11-23 09:29:14 -0500

    formatDump stub

---
+++ scripts/formatDump.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+/**
+ * User: daveb
+ * Date: 11/23/15
+ * Time: 9:27 AM
+ */
+
+/**
+ * @todo next up is a script to do that on a cron
+ *      1. convert all existing backticks to apostrophes
+ *      2. convert the group "|:|" to backtick
+ *      3. ?
+ *      4. Go have a sandwich.
+ *      5. Think about how you wish that you could do something about the initial export
+ */
