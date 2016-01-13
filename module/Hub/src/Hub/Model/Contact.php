cded4a68d4b74df72a75a73e3ae5c0aa9964a4d6
dbullard <dbullard@bizjournals.com>
2016-01-13 16:07:12 -0500

    magicMethod docs on contact
    refs #35032

--- module/Hub/src/Hub/Model/Contact.php
+++ module/Hub/src/Hub/Model/Contact.php
@@ -9,11 +9,34 @@ namespace Hub\Model;
 
 use \Entity\Model\Base;
 
+
 /**
  * Class Contact
  * @package Hub\Model
- *
- * @method getHubId() return hub_id
+ * Documentation for magic methods - implemented in Core_Model_SchemaBase
+ * @method integer   getContactId() schema data get method
+ * @method integer   getHubId() schema data get method
+ * @method integer   getMeroveusId() schema data get method
+ * @method integer   getRelevateId() schema data get method
+ * @method integer   getIsDuplicate() schema data get method
+ * @method integer   getIsCurrentEmployee() schema data get method
+ * @method string   getFirstName() schema data get method
+ * @method string   getMiddleInitial() schema data get method
+ * @method string   getLastName() schema data get method
+ * @method string   getSuffix() schema data get method
+ * @method string   getHonorific() schema data get method
+ * @method string   getJobTitle() schema data get method
+ * @method integer   getJobPositionId() schema data get method
+ * @method string   getEmail() schema data get method
+ * @method string   getPhone() schema data get method
+ * @method string   getAddress1() schema data get method
+ * @method string   getAddress2() schema data get method
+ * @method string   getCity() schema data get method
+ * @method string   getState() schema data get method
+ * @method string   getPostalCode() schema data get method
+ * @method string   getCreatedAt() schema data get method
+ * @method string   getUpdatedAt() schema data get method
+ * @method string   getELETEDAt() schema data get method
  */
 class Contact extends Base
 {
