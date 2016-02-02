ff350d1a1f2d5da51465a646b6bda1e574d36554
justinrobinson <justinrobinson@bizjournals.com>
2016-02-02 09:31:31 -0500

    [develop] move action controller logic to our own abstract instead of the zend abstract in vendor

--- vendor/zendframework/zendframework/library/Zend/Mvc/Controller/AbstractActionController.php
+++ vendor/zendframework/zendframework/library/Zend/Mvc/Controller/AbstractActionController.php
@@ -26,35 +26,6 @@ abstract class AbstractActionController extends AbstractController
     protected $eventIdentifier = __CLASS__;
 
     /**
-     * @var $sqlStatementsArray \mysqli_stmt[]
-     */
-    protected $sqlStatementsArray = [];
-
-    /**
-     * @var $sqlStringsArray string[]
-     */
-    protected $sqlStringsArray = [];
-
-    /**
-     * @var \PDO
-     */
-    protected $db;
-
-    public function __construct () {
-
-        // setup db connection
-        // todo dynamic db connection
-        $this->db = new \PDO('mysql:host=devdb.bizjournals.int;dbname=datahub', 'web', '');
-        $this->db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-
-        // prepare sql statements
-        foreach ( $this->sqlStringsArray as $name => $sqlString ) {
-            $this->sqlStatementsArray[$name] = $this->db->prepare( $sqlString );
-            $this->sqlStatementsArray[$name]->setFetchMode(\PDO::FETCH_ASSOC);
-        }
-    }
-
-    /**
      * Default action if none provided
      *
      * @return array
