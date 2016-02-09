6f1fd4f11a415b4e29aac85cf973df955c779b68
dbullard <dbullard@bizjournals.com>
2016-02-09 12:58:06 -0500

    remove "noData" from insert formatter
    refs #DATA-36

--- module/Console/src/Console/Record/Formatter/formatters/meroveus.php
+++ module/Console/src/Console/Record/Formatter/formatters/meroveus.php
@@ -19,17 +19,17 @@ class Meroveus {
 
         return [
 
-            ':refinery_id'        => isset( $data['refinery_id'] ) ? $data['refinery_id'] : 'noData',
-            ':meroveus_id'        => isset( $data['meroveusId'] ) ? $data['meroveusId'] : 'noData',
-            ':generate_code'      => isset( $data['generate_codeId'] ) ? $data['generate_codeId'] : 'noData',
-            ':record_source'      => isset( $data['sourceId'] ) ? $data['sourceId'] : 'noData',
-            ':company_name'       => isset( $data['firm-name_static'] ) ? $data['firm-name_static'] : 'noData',
-            ':public_ticker'      => isset( $data['idk'] ) ? $data['idk'] : 'noData',
-            ':ticker_exchange'    => isset( $data['idk'] ) ? $data['idk'] : 'noData',
-            ':source_modified_at' => isset( $data['idk'] ) ? $data['idk'] : 'noData',
-            ':address1'           => isset( $data['street-address_static'] ) ? $data['street-address_static'] : 'noData',
-            ':address2'           => isset( $data['street-line2-address_static'] ) ? $data['street-line2-address_static'] : 'noData',
-            ':city'               => isset( $data['street-city_static'] ) ? $data['street-city_static'] : 'noData',
+            ':refinery_id'        => isset( $data['refinery_id'] ) ? $data['refinery_id'] : null,
+            ':meroveus_id'        => isset( $data['meroveusId'] ) ? $data['meroveusId'] : null,
+            ':generate_code'      => isset( $data['generate_codeId'] ) ? $data['generate_codeId'] : null,
+            ':record_source'      => isset( $data['sourceId'] ) ? $data['sourceId'] : null,
+            ':company_name'       => isset( $data['firm-name_static'] ) ? $data['firm-name_static'] : '',
+            ':public_ticker'      => isset( $data['idk'] ) ? $data['idk'] : null,
+            ':ticker_exchange'    => isset( $data['idk'] ) ? $data['idk'] : null,
+            ':source_modified_at' => isset( $data['idk'] ) ? $data['idk'] : null,
+            ':address1'           => isset( $data['street-address_static'] ) ? $data['street-address_static'] : null,
+            ':address2'           => isset( $data['street-line2-address_static'] ) ? $data['street-line2-address_static'] : null,
+            ':city'               => isset( $data['street-city_static'] ) ? $data['street-city_static'] : null,
             ':state'              => isset( $data['street-state_static'] ) ? $data['street-state_static'] : null,
             ':postal_code'        => isset( $data['street-zip_static'] ) ? $data['street-zip_static'] : null, // validate
             ':country'            => isset( $data['street-country_static'] ) ? $data['street-country_static'] : null, // validate
