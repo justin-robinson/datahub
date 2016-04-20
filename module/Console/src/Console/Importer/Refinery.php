227a7f086bb74d678bb1694ff85f4e297504bb20
justinrobinson <justinrobinson@bizjournals.com>
2016-04-20 10:26:31 -0400

    [feature-cron-jobs] comments

--- module/Console/src/Console/Importer/Refinery.php
+++ module/Console/src/Console/Importer/Refinery.php
@@ -30,9 +30,13 @@ class Refinery extends ImporterAbstract {
         // how many rows we processed
         $count = 0;
 
-        // There is limit 65,535 (2^16-1) place holders in prepared sql statements in mariadb
-        // we have 21 per insert which gives us 3,120 max buffered inserts
+        // how many inserts we will do at once
+        $bufferLimit = 1000;
+
+        // the table we are inserting into
         $bufferTable = 'datahub.company';
+
+        // names of columns to insert
         $columnNames = [
             'refinery_id',
             'meroveus_id',
@@ -59,9 +63,12 @@ class Refinery extends ImporterAbstract {
             'updated_at',
             'deleted_at',
         ];
-        $bufferLimit = 1000;
+
+        // pdo sql placeholders string
+        // notice last three are hardcoded to NOW(), NOW(), and 0
         $sqlValuesTemplate = '(' . implode( ',', array_fill( 0, count( $columnNames ) - 3, '?' ) ) . ', NOW(), NOW(), 0)';
 
+        // make dat buffer
         $queryBuffer = new Buffer( $bufferLimit, $db, $bufferTable, $columnNames, $sqlValuesTemplate );
 
         // data formatter
@@ -74,6 +81,8 @@ class Refinery extends ImporterAbstract {
                 // why don't we merge automatically?
                 // because then we would have to try catch around the foreach loop and that would
                 // cause the loop to break.  This way we can continue processing the remaining rows
+
+                // format the record and "insert" it into the db
                 $queryBuffer->insert(
                     $formatter->format(
                         $file->mergeWithHeaderRow( $record )
@@ -89,6 +98,7 @@ class Refinery extends ImporterAbstract {
 
         }
 
+        // flush remaining inserts left in buffer
         $queryBuffer->flush();
 
         return $count;
