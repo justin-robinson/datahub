c103b08c99cf2473920bc38dde7f00a77c0524dd
justinrobinson <justinrobinson@bizjournals.com>
2016-05-05 15:25:48 -0400

    [feature-data-overhaul] add address and mapping table

---
+++ scoop-classes/db/datahub/address.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DB\Datahub;
+
+/**
+ * Class Address
+ * @package DB\Datahub
+ * @author jrobinson (robotically)
+ * @date 2016/05/05
+ * @inheritdoc
+ * This file is only generated once
+ * Put your class specific code in here
+ */
+class Address extends \DBCore\Datahub\Address {
+
+
+
+
+
+
+}
+
+?>
