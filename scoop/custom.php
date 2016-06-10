afe56d5918e14691245a3705fcbcaafa118ef8a9
justinrobinson <justinrobinson@bizjournals.com>
2016-06-10 12:58:18 -0400

    [feature-data-overhaul] fix for tech

--- scoop/custom.php
+++ scoop/custom.php
@@ -2,5 +2,5 @@
 
 $docRoot = \Scoop\Config::get_option( 'server_document_root' );
 if ( $docRoot ) {
-    \Scoop\Config::set_option( 'server_document_root', $docRoot . '/../scoop' );
+    \Scoop\Config::set_option( 'server_document_root', __DIR__ );
 }
