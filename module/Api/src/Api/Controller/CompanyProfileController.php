7cbdf649555197a74d589b0dc99765db89d178ac
dbullard <dbullard@bizjournals.com>
2016-06-21 15:55:17 -0400

    hacking channel ids into life on the refinery stuff

--- module/Api/src/Api/Controller/CompanyProfileController.php
+++ module/Api/src/Api/Controller/CompanyProfileController.php
@@ -10,6 +10,7 @@ namespace Api\Controller;
 use Api\ResponseFormatter\CompanyProfileCollectionFormatter;
 use Api\ResponseFormatter\CompanyProfileFormatter;
 use Api\Response\CompanyResponse;
+use Api\ResponseFormatter\InstanceFormatter;
 use DB\Datahub\Company;
 use DB\Datahub\CompanyInstance;
 use Zend\View\Model\JsonModel;
@@ -24,15 +25,16 @@ class CompanyProfileController extends AbstractRestfulController
     /**
      * @param mixed $companyId
      *
-*@return JsonModel
+     * @return JsonModel
      */
-    public function get( $companyId )
+    public function get($companyId)
     {
-        
+
         $company = Company::fetch_company_and_instances($companyId);
 
-        if ( $company === false ) {
+        if ($company === false) {
             $this->response->setStatusCode(204);
+
             return null;
         }
 
@@ -47,6 +49,7 @@ class CompanyProfileController extends AbstractRestfulController
     public function delete($companyId)
     {
         $this->response->setStatusCode(405);
+
         return new JsonModel(['message' => 'not allowed']);
     }
 
@@ -59,6 +62,7 @@ class CompanyProfileController extends AbstractRestfulController
     public function create($data)
     {
         $this->response->setStatusCode(405);
+
         return new JsonModel(['message' => 'not allowed']);
     }
 
@@ -71,6 +75,7 @@ class CompanyProfileController extends AbstractRestfulController
     public function update($id, $data)
     {
         $this->response->setStatusCode(405);
+
         return new JsonModel(['message' => 'not allowed']);
     }
 
@@ -79,25 +84,26 @@ class CompanyProfileController extends AbstractRestfulController
      */
     public function getList()
     {
-        $from = isset($_GET['from']) ? $_GET['from'] : '0';
-        $to = isset($_GET['to']) ? $_GET['to'] : date('Y-m-d H:i:s');
-        $page = (isset($_GET['page']) && (int)$_GET['page'] >= 1 ) ? $_GET['page'] : 1;
-        $limit = isset($_GET['limit']) ? $_GET['limit'] : 1000;
-        $offset = $limit * ($page-1);
-        $companies = Company::fetch_modified_in_range( $from, $to, $offset, $limit);
+        $from      = isset($_GET['from']) ? $_GET['from'] : '0';
+        $to        = isset($_GET['to']) ? $_GET['to'] : date('Y-m-d H:i:s');
+        $page      = (isset($_GET['page']) && (int)$_GET['page'] >= 1) ? $_GET['page'] : 1;
+        $limit     = isset($_GET['limit']) ? $_GET['limit'] : 1000;
+        $offset    = $limit * ($page - 1);
+        $companies = Company::fetch_modified_in_range($from, $to, $offset, $limit);
 
-        if ( $companies->get_num_rows() === 0 ) {
+        if ($companies->get_num_rows() === 0) {
             $this->response->setStatusCode(204);
+
             return null;
         }
 
-        foreach ( $companies as $company ) {
+        foreach ($companies as $company) {
             /**
              * @var $company Company
              */
             $company->fetch_company_instances();
 
-            foreach ( $company->get_company_instances() as $instance ) {
+            foreach ($company->get_company_instances() as $instance) {
                 /**
                  * @var $instance CompanyInstance
                  */
@@ -109,6 +115,7 @@ class CompanyProfileController extends AbstractRestfulController
         }
 
         $count = $companies->get_num_rows();
+
         return new JsonModel(CompanyProfileCollectionFormatter::format($companies, $page, $limit, $count, $from, $to));
     }
 
@@ -128,7 +135,7 @@ class CompanyProfileController extends AbstractRestfulController
 
         $response = new CompanyResponse();
 
-        if ( $company ) {
+        if ($company) {
 
             // the company instances
             $company->fetch_company_instances();
@@ -136,8 +143,9 @@ class CompanyProfileController extends AbstractRestfulController
             // sorted list of properties
             $sortedProperties = [];
 
+            $channelIds = [];
             // get and sort all properties and contacts
-            foreach ( $company->get_company_instances() as $instance ) {
+            foreach ($company->get_company_instances() as $instance) {
 
                 // get
                 $instance->fetch_properties();
@@ -147,14 +155,24 @@ class CompanyProfileController extends AbstractRestfulController
 
                 // contacts
                 $instance->fetch_contacts();
+
+                $instance->fetch_channel_ids();
+
+                $instance->get_channel_ids();
+
+
+                foreach ($instance->get_channel_ids() as $channelId) {
+                    $channelIds[] = $channelId->channel_id;
+                }
+
             }
 
             // convert to array
             $company = $company->to_array();
-
+            $company['channelIds'] = $channelIds;
             // replace instance properties with sorted ones
             reset($sortedProperties);
-            foreach ( $company['instances'] as &$instance ) {
+            foreach ($company['instances'] as &$instance) {
                 $instance['properties'] = current($sortedProperties);
                 next($sortedProperties);
             }
@@ -170,7 +188,6 @@ class CompanyProfileController extends AbstractRestfulController
 
         return new JsonModel($response->toArray());
     }
-
 
 
 }
