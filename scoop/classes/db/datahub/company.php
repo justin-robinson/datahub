53947b09261b4c5143413028f50f85d20e0e2ade
justinrobinson <justinrobinson@bizjournals.com>
2016-07-07 09:10:34 -0400

    [feature-release-20160714] #RELEASE-20160714 readd the utf8 encoding logic for company data

--- scoop/classes/db/datahub/company.php
+++ scoop/classes/db/datahub/company.php
@@ -140,7 +140,7 @@ class Company extends \DBCore\Datahub\Company
             $this->normalizedName = strtolower($normalizedName);
         }
 
-        parent::__set($name, $value);
+        parent::__set($name, is_scalar($value) ? utf8_encode($value) : $value);
     }
 
     /**
