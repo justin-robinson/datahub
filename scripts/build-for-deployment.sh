d5efb09b3a4da99bfdd83ca0b89da1d1955e31ae
justinrobinson <justinrobinson@bizjournals.com>
2016-07-20 11:30:35 -0400

    [feature-DATA-530] #DATA-530 build script for deployment

--- scripts/build-for-deployment.sh
+++ scripts/build-for-deployment.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+Color_Off="\033[0m"       # Text Reset
+BBlack="\033[1;30m"       # Black
+On_Green="\033[42m"       # Green
+
 # cd up one directory from this script to the datahub root
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/../public
+cd ${DIR}/..
 
 echo "working in " `pwd`
 
 # get dependencies
-bower install
+echo -e ${BBlack}${On_Green}"Installing dependencies"${Color_Off}
+bower cache clean
+bower install --production
 
 # build project
-polymer build --html.collapseWhitespace
+echo -e ${BBlack}${On_Green}"Building"${Color_Off}
+(cd public && exec polymer build --html.collapseWhitespace)
+rsync -a public/build/unbundled/ public/
