efcae95ae66f1690b9a232102585aaf4589643dc
justinrobinson <justinrobinson@bizjournals.com>
2016-08-01 12:08:18 -0400

    [feature-DATA-571] #DATA-571 hellacious bulk post/put api endpoint

--- scoop/classes/db/datahub/companyinstanceproperty.php
+++ scoop/classes/db/datahub/companyinstanceproperty.php
@@ -27,6 +27,10 @@ class CompanyInstanceProperty extends \DBCore\Datahub\CompanyInstanceProperty
      */
     public function __set($name, $value)
     {
+        if ( $name === 'value' ) {
+
+            $this->valueMd5 = md5($value);
+        }
 
         parent::__set($name, is_scalar($value) ? utf8_encode($value) : $value);
     }
