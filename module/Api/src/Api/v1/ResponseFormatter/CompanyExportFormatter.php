242440989cc368ce377b841d0e99bc7979b65b58
justinrobinson <justinrobinson@bizjournals.com>
2016-08-09 16:40:04 -0400

    [feature-DATA-582] #DATA-582 hal on company export

--- module/Api/src/Api/v1/ResponseFormatter/CompanyExportFormatter.php
+++ module/Api/src/Api/v1/ResponseFormatter/CompanyExportFormatter.php
@@ -6,6 +6,7 @@ use DB\Datahub\Company;
 use DB\Datahub\CompanyInstance;
 use DB\Datahub\CompanyInstanceProperty;
 use DB\Datahub\SourceType;
+use Scoop\Database\Model\Generic;
 use Scoop\Database\Rows;
 
 /**
@@ -16,14 +17,83 @@ class CompanyExportFormatter
 {
 
     /**
-     * @param Rows $companies
-     * $companies
+     * @param Rows   $companies
+     * @param        $page
+     * @param        $limit
+     * @param        $totalCount
+     * @param        $from
+     * @param        $to
+     * @param string $apiPath
      *
      * @return array
      */
-    public static function format(Rows $companies)
-    {
-        $array = [];
+    public static function format(
+        Rows $companies,
+        $page,
+        $limit,
+        $totalCount,
+        $from,
+        $to,
+        $apiPath = '/api/v1/company/export'
+    ) {
+        $host = FormatterHelpers::get_http_protocol() . FormatterHelpers::get_server_variable('HTTP_HOST');;
+        $uri = $host . $apiPath;
+        $totalCount = is_null($totalCount) ? Generic::query('SELECT count(*) AS count FROM company')->first()->count : $totalCount;
+        $lastPage = ceil($totalCount / $limit);
+
+        $path = parse_url(FormatterHelpers::get_server_variable('REQUEST_URI'), PHP_URL_PATH);
+        $selfQueryParams = [
+            'from'  => $from,
+            'to'    => $to,
+            'limit' => $limit,
+            'page'  => $page,
+        ];
+
+        $firstQueryParams = $selfQueryParams;
+        $firstQueryParams['page'] = 1;
+
+        $lastQueryParams = $selfQueryParams;
+        $lastQueryParams['page'] = $lastPage;
+
+        $array = [
+            'count'     => [
+                'total'    => $totalCount,
+                'current'  => $companies->get_num_rows(),
+                'offset'   => ($page - 1) * $limit,
+                'nextPage' => ($page < $lastPage) ? $page + 1 : null,
+                'lastPage' => $lastPage,
+            ],
+            '_links'    => [
+                'self'  => [
+                    'href' => $host . $path . '?' . http_build_query($selfQueryParams),
+                ],
+                'first' => [
+                    'href' => $uri . '?' . http_build_query($firstQueryParams),
+                ],
+                'last'  => [
+                    'href' => $uri . '?' . http_build_query($lastQueryParams),
+                ],
+            ],
+            '_embedded' => [
+                'companies' => [],
+            ],
+        ];
+
+        if ($page > 1) {
+            $prevQueryParams = $selfQueryParams;
+            $prevQueryParams['page'] = $page - 1;
+            $array['_links']['prev'] = [
+                'href' => $uri . '?' . http_build_query($prevQueryParams),
+            ];
+        }
+
+        if ($page < $lastPage) {
+            $nextQueryParams = $selfQueryParams;
+            $nextQueryParams['page'] = $page + 1;
+            $array['_links']['next'] = [
+                'href' => $uri . '?' . http_build_query($nextQueryParams),
+            ];
+        }
 
         foreach ( $companies as $company ) {
             /**
@@ -116,7 +186,7 @@ class CompanyExportFormatter
             $companyRecord['refineryIds'] = array_keys($companyRecord['refineryIds']);
             $companyRecord['instanceIds'] = array_keys($companyRecord['instanceIds']);
             $companyRecord['industries'] = array_keys($companyRecord['industries']);
-            $array[$company->companyId] = $companyRecord;
+            $array['_embedded']['companies'][] = $companyRecord;
         }
 
         return array_values($array);
