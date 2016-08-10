3079b025653467df59c2ce88aedd3b2e42e7ea42
dbullard <dbullard@bizjournals.com>
2016-08-10 14:17:57 -0400

    initial commit
    #DATA-546

---
+++ module/Api/src/Api/v1/ResponseFormatter/Top25ListCollectionFormatter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dbullard
+ * Date: 8/10/16
+ * Time: 2:05 PM
+ */
+
+namespace Api\v1\ResponseFormatter;
+
+
+class Top25ListCollectionFormatter
+{
+    
+}
