b415455c0b7176cfb7a602bc43a46cf8ae177b2d
dbullard <dbullard@bizjournals.com>
2016-08-11 13:38:18 -0400

    new model for the lists. post now takes an array of meroveusIds
    #DATA-546

--- module/Api/src/Api/v1/Controller/Top25ListController.php
+++ module/Api/src/Api/v1/Controller/Top25ListController.php
@@ -10,11 +10,13 @@ namespace Api\v1\Controller;
 
 use Api\v1\ResponseFormatter\Top25ListCollectionFormatter;
 use DB\Datahub\CompanyInstanceTop25lists;
+use DB\Datahub\Top25List;
 use DB\Datahub\Company;
 use DB\Datahub\CompanyInstance;
 
 use Scoop\Database\Model\Generic;
 use Zend\Db\Sql\Ddl\Column\Integer;
+use Zend\Json\Json;
 use Zend\View\Model\JsonModel;
 
 /**
@@ -28,14 +30,17 @@ class Top25ListController extends AbstractRestfulController
      * @param $companyInstanceId
      *
      * @return JsonModel
+     *
+     * useful for debugging since we want to get this payload with the company return
      */
-    public function get($companyInstanceId){
+    public function get($companyInstanceId)
+    {
         $lists = CompanyInstanceTop25lists::fetch_where('companyInstanceId = ?', [$companyInstanceId]);
         if ($lists) {
             
             return new JsonModel(Top25ListCollectionFormatter::format($lists));
         }
-
+        
         return $this->getResponse()->setStatusCode(204);
         
     }
@@ -43,11 +48,14 @@ class Top25ListController extends AbstractRestfulController
     /**
      * here's an example of the body contained in $data
      * {
-     *   "companyMeroveusId": 11111,
-     *   "listId":            1111,
-     *   "issueDate":         "",
-     *   "pageHeadline":      "",
-     *   "listUrl":           ""
+     * "listId":            323423444,
+     * "companyMeroveusIds": [
+     * 265504,
+     * 564178
+     * ],
+     * "issueDate":         "",
+     * "pageHeadline":      "this came from a post as well",
+     * "listUrl":           "/google.html"
      * }
      *
      * @param $data
@@ -59,20 +67,49 @@ class Top25ListController extends AbstractRestfulController
      */
     public function create($data)
     {
-        $instance = CompanyInstance::fetch_by_source_name_and_id('meroveus', $data['companyMeroveusId']);
-        
-        $list = new CompanyInstanceTop25lists();
-        $list->companyInstanceId = $instance->first()->companyInstanceId;
-        $list->listId = $data['listId'];
-        $list->issueDate = $data['issueDate'];
-        $list->pageHeadline = $data['pageHeadline'];
-        $list->listUrl = $data['listUrl'];
-        
-        
-        if ($list->save()) {
-            $list->reload();
+        if (empty($data['companyMeroveusIds'])) {
+            $this->getResponse()->setStatusCode(500);
+            return new JsonModel(['message' => 'missing company ids']);
         }
         
-        return new JsonModel($list->to_array());
+        // do the mappings
+        foreach ($data['companyMeroveusIds'] as $id) {
+            // get the companyInstanceId
+            $instance = CompanyInstance::fetch_by_source_name_and_id('meroveus', $id);
+            // if we ever decide to implement this:
+//            if (!$instance) {
+//                $this->getResponse()->setStatusCode(500);
+//                return new JsonModel(['message' => 'instance with meroveus_id#' . $id . ' not present in datahub']);
+//            }
+            
+            if($instance){
+                // build the mapping
+                $map                    = new CompanyInstanceTop25lists();
+                $map->companyInstanceId = $instance->first()->companyInstanceId;
+                $map->listId            = $data['listId'];
+                // save the mapping
+                $mapSave = $map->save();
+    
+                if (!$mapSave) {
+                    $this->getResponse()->setStatusCode(500);
+                    return new JsonModel(['message' => 'mapping did not save']);
+                }
+            }
+        }
+        
+        // build the list
+        $list               = new Top25List();
+        $list->listId       = $data['listId'];
+        $list->issueDate    = $data['issueDate'];
+        $list->pageHeadline = $data['pageHeadline'];
+        $list->listUrl      = $data['listUrl'];
+        // save the list
+        if ($list->save()) {
+            $list->reload();
+            return new JsonModel($list->to_array());
+        } else {
+            $this->getResponse()->setStatusCode(500);
+            return new JsonModel(['message' => 'list failed to sve']);
+        }
     }
 }
---
+++ scoop/classes/db/datahub/top25list.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DB\Datahub;
+
+/**
+ * Class Top25List
+ * @package DB\Datahub
+ * @author jrobinson (robotically)
+ * @date 2016/08/11
+ * @inheritdoc
+ * This file is only generated once
+ * Put your class specific code in here
+ */
+class Top25List extends \DBCore\Datahub\Top25List {
+
+}
+
+?>
