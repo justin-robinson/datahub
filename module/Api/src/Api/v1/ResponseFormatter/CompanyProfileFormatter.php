b5d9a6337163ed1ebdfe75e2608318dda9f87891
justinrobinson <justinrobinson@bizjournals.com>
2016-08-18 11:22:30 -0400

    [feature-DATA-587] #DATA-587 best properties on company profile api

--- module/Api/src/Api/v1/ResponseFormatter/CompanyProfileFormatter.php
+++ module/Api/src/Api/v1/ResponseFormatter/CompanyProfileFormatter.php
@@ -4,6 +4,9 @@ namespace Api\v1\ResponseFormatter;
 
 use DB\Datahub\Company;
 use DB\Datahub\CompanyInstance;
+use DB\Datahub\CompanyInstanceProperty;
+use DB\Datahub\DhIndustryBizjChannelMap;
+use DB\Datahub\SourceType;
 
 /**
  * Class CompanyProfileFormatter
@@ -22,6 +25,80 @@ class CompanyProfileFormatter
 
         // format company into hal response
         $array = CompanyFormatter::format($company);
+
+        /**
+         * @var $latestInstance CompanyInstance
+         * @var $latestProperty CompanyInstanceProperty[]
+         */
+        // the last instance to be updated
+        $latestInstance = $company->get_latest_instance();
+
+        $array['properties'] = [
+            'updatedAt'   => ($array['updatedAt'] < $latestInstance->updatedAt) ? $latestInstance->updatedAt : $array['updatedAt'],
+            'externalIds' => [],
+            'industries'  => [],
+            'channelIds'  => [],
+        ];
+
+        // array ( indexed by source type id ) of property source types
+        $sourceTypes = [];
+        foreach ( SourceType::fetch_all() as $sourceType ) {
+            /**
+             * @var $sourceType SourceType
+             */
+            $sourceTypes[$sourceType->sourceTypeId] = $sourceType;
+        }
+
+        // add all properties from the latest instance
+        foreach ( $latestInstance->get_properties() as $property) {
+            $array['properties'][$property->name] = $property->value;
+        }
+
+        foreach ( $company->get_company_instances() as $instance ) {
+            /**
+             * @var $instance CompanyInstance
+             */
+
+            // get the channel ids for each instance
+            foreach ( $instance->get_channel_ids() as $channelId ) {
+                /**
+                 * @var $channelId DhIndustryBizjChannelMap
+                 */
+                $array['properties']['channelIds'][$channelId->channel_id] = true;
+            }
+
+            // add this instance id
+            $array['properties']['externalIds']['instance'][$instance->companyInstanceId] = true;
+
+            foreach ( $instance->get_properties() as $property ) {
+                /**
+                 * @var $property CompanyInstanceProperty
+                 */
+                // update the updateAt if this property is newer than the company or instance
+                $array['properties']['updatedAt'] = ($array['properties']['updatedAt'] < $property->updatedAt) ? $property->updatedAt : $array['properties']['updatedAt'];
+
+                // add this external id
+                if ( array_key_exists($property->sourceTypeId, $sourceTypes) ) {
+
+                    preg_match('/([\w]*):.*/', $sourceTypes[$property->sourceTypeId]->name, $matches);
+                    // but only refinery ids
+                    if ( array_key_exists(1, $matches) ) {
+                        $array['properties']['externalIds'][$matches[1]][$property->sourceId] = true;
+                    }
+                }
+
+                // add industry
+                if ( $property->name === 'industry' ) {
+                    $array['properties']['industries'][$property->value] = true;
+                }
+            }
+        }
+
+        foreach ( $array['properties']['externalIds'] as &$externalIds ) {
+            $externalIds = array_keys($externalIds);
+        }
+        $array['properties']['industries'] = array_keys($array['properties']['industries']);
+        $array['properties']['channelIds'] = array_keys($array['properties']['channelIds']);
 
         $array['instances'] = [];
 
