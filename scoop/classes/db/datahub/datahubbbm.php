76d5c64e285263cdafdeea9c55b911addd7179b4
dbullard <dbullard@bizjournals.com>
2016-09-12 16:37:53 -0400

    working "get by dh id" bbm endpoint
    #DATA-661

--- scoop/classes/db/datahub/datahubbbm.php
+++ scoop/classes/db/datahub/datahubbbm.php
@@ -14,24 +14,6 @@ namespace DB\Datahub;
  */
 class DatahubBbm extends \DBCore\Datahub\DatahubBbm
 {
-    
-    public function populate(array $data)
-    {
-        if (empty ($data)) {
-            return $this;
-        } else {
-            $this->dhInstanceId  = empty($data[0]) ? null : $data[0];
-            $this->bbmOrgAddress = empty($data[1]) ? null : $data[1];
-            $this->bbmId         = empty($data[2]) ? null : $data[2];
-            $this->bbmOrgUrl     = empty($data[3]) ? null : $data[3];
-            $this->bbmOrgName    = empty($data[4]) ? null : $data[4];
-            $this->bbmOrgCity    = empty($data[5]) ? null : $data[5];
-            $this->bbmOrgState   = empty($data[6]) ? null : $data[6];
-//            $this->bbmOrgZip     = empty($data[6]) ? null : $data[6];
-            return $this;
-        }
-    }
-    
 }
 
 ?>
